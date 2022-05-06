a7a34689d343ab12ab724836dd2ec66e797086af
raisul8741 <r.raisulislamshovon@gmail.com>
2022-05-06 18:48:49 +0600

    gitAction: trying to runtest

--- test_feature/step_definitions/ui/browser.js
+++ test_feature/step_definitions/ui/browser.js
@@ -2,8 +2,20 @@ const { Given } = require('@cucumber/cucumber');
 const web_driver = require('selenium-webdriver');
 
 Given(/^I open a browser$/, async function () {
+    // this.driver = new web_driver.Builder()
+    //     .forBrowser(this.platform)
+    //     .build();
+    // this.driver.manage().window().maximize();
+    // this.isBrowserOpen = true;
+
+
+    const screen = {
+        width: 1920,
+        height: 1080
+    };
     this.driver = new web_driver.Builder()
         .forBrowser(this.platform)
+        .setChromeOptions(new chrome.Options().headless().windowSize(screen))
         .build();
     this.driver.manage().window().maximize();
     this.isBrowserOpen = true;
